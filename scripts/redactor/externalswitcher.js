b2aa289893aa5ba3293af6d14912d63bc883b1ab
Rikard Ekberg <rikard.ekberg@gmail.com>
2013-01-26 07:33:39 +0100

    Cleaned up the script and added 'use strict'

--- scripts/redactor/externalswitcher.js
+++ scripts/redactor/externalswitcher.js
@@ -2,13 +2,14 @@ if (typeof RedactorPlugins === 'undefined') var RedactorPlugins = {};
 if (typeof RedactorToolbars === 'undefined') var RedactorToolbars = [];
 
 (function($){
+  'use strict';
   RedactorPlugins.externalswitcher = {
     init: function() {
       RedactorToolbars.push(this.$toolbar);
 
       $(this.$toolbar).hide();
       
-      $(this.$editor).focus($.proxy(function (event) {
+      $(this.$editor).focus($.proxy(function () {
         for(var i = 0; i < RedactorToolbars.length; i++) {
           $(RedactorToolbars[i]).hide();
         }
